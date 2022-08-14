27a0ffa5f95f49919f8f62dac53988b7aee731fc
Lovecraftian Horror <LovecraftianHorror@pm.me>
2022-08-13 20:53:27 -0500

    Gracefully handle no config file

--- src/cli.rs
+++ src/cli.rs
@@ -53,10 +53,14 @@ pub struct Config {
 impl Config {
     pub fn load() -> anyhow::Result<Self> {
         let config_dir = dirs::config_dir().context("Failed detecting config dir")?;
-        let text = fs::read_to_string(&config_dir.join("inlyne").join("inlyne.toml"))
-            .context("Failed reading config file")?;
-        let config = toml::from_str(&text)?;
-        Ok(config)
+        let config_path = config_dir.join("inlyne").join("inlyne.toml");
+        if config_path.is_file() {
+            let text = fs::read_to_string(&config_path).context("Failed reading config file")?;
+            let config = toml::from_str(&text)?;
+            Ok(config)
+        } else {
+            Ok(Self::default())
+        }
     }
 }
 
