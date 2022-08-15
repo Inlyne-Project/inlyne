fa3fb1f4fbfa7980c30a612ea39b6a875a29bb30
CosmicHorror <LovecraftianHorror@pm.me>
2022-08-15 00:36:55 -0500

    Initial codeblock syntax highlighting support (#9)
    Using the syntect code highlighting to colour code blocks. Also fixes layout issues with code blocks.

--- src/color.rs
+++ src/color.rs
@@ -18,6 +18,7 @@ pub struct Theme {
     pub code_block_color: [f32; 4],
     pub link_color: [f32; 4],
     pub select_color: [f32; 4],
+    pub code_highlighter: &'static str,
 }
 
 pub const DARK_DEFAULT: Theme = Theme {
@@ -28,17 +29,19 @@ pub const DARK_DEFAULT: Theme = Theme {
         b: 0.0086,
         a: 1.0,
     },
-    code_color: [1., 0.0578, 0.9331, 1.0],
+    code_color: [1. * 0.7, 0.0578 * 0.7, 0.9331 * 0.7, 1.0],
     code_block_color: [0.0080 * 1.5, 0.0110 * 1.5, 0.0156 * 1.5, 1.0],
     link_color: [0.0976, 0.3813, 1.0, 1.0],
     select_color: [0.17, 0.22, 0.3, 1.0],
+    code_highlighter: "base16-mocha.dark",
 };
 
 pub const LIGHT_DEFAULT: Theme = Theme {
     text_color: [0., 0., 0., 1.0],
     clear_color: wgpu::Color::WHITE,
     code_color: [1., 0.0578, 0.9331, 1.0],
-    code_block_color: [0.9, 0.9, 0.9, 1.0],
+    code_block_color: [0.92, 0.92, 0.92, 1.0],
     link_color: [0.0975, 0.1813, 1.0, 1.0],
     select_color: [0.67, 0.85, 0.9, 1.0],
+    code_highlighter: "InspiredGitHub",
 };
