cc9c1dab55fec6771db41527404c7e75e048341c
Lucas Timmins <timmins.s.lucas@gmail.com>
2022-10-14 19:34:25 +0800

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -468,7 +468,7 @@ impl Inlyne {
                                     let maybe_path = PathBuf::from_str(link).ok();
                                     let is_local_md = maybe_path.as_ref().map_or(false, |p| {
                                         p.extension().map_or(false, |ext| ext == "md")
-                                        && !p.to_str().map_or(false, |s| s.starts_with("http"))
+                                            && !p.to_str().map_or(false, |s| s.starts_with("http"))
                                     });
                                     if is_local_md {
                                         // Open markdown files ourselves
@@ -479,8 +479,11 @@ impl Inlyne {
                                         // parent
                                         let maybe_path = if maybe_path.is_relative() {
                                             // Simply canonicalizing it doesn't suffice and leads to "no such file or directory"
-                                            let current_parent = args.file_path.parent().expect("no current parent");
-                                            let link_without_prefix: &Path = maybe_path.strip_prefix(std::path::Component::CurDir).expect("no CurDir prefix");
+                                            let current_parent =
+                                                args.file_path.parent().expect("no current parent");
+                                            let link_without_prefix: &Path = maybe_path
+                                                .strip_prefix(std::path::Component::CurDir)
+                                                .expect("no CurDir prefix");
                                             let mut link = current_parent.to_path_buf();
                                             link.push(link_without_prefix);
                                             link
