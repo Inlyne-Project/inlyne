387108e446570d2d1f22a2054fa43e277dda406a
Valentin271 <36198422+Valentin271@users.noreply.github.com>
2023-12-21 08:39:01 +0100

    fix: filter file watcher duplicate events (#200)
    * refactor: log millis and added logging
    
    * fix: use notify-debouncer-full to filter multiple events
    
    * chore: fmt
    
    * Send notifications for file creation too
    
    * Use `inlyne=trace` instead of debug for tests
    
    * Fix renames
    
    * Drain any initial notifications
    
    ---------
    
    Co-authored-by: Cosmic Horror <CosmicHorrorDev@pm.me>

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -3,7 +3,7 @@ use log::LevelFilter;
 pub fn init_test_log() {
     // Ignore errors because other tests in the same binary may have already initialized the logger
     let _ = env_logger::Builder::new()
-        .filter(Some("inlyne"), LevelFilter::Debug)
+        .filter(Some("inlyne"), LevelFilter::Trace)
         .filter(None, LevelFilter::Warn)
         .is_test(true)
         .try_init();
