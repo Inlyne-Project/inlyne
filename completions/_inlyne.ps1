fef8975447fe1135895e0a35659cb83c0bdc68ca
Cosmic Horror <CosmicHorrorDev@pm.me>
2024-02-17 11:02:33 -0700

    chore: `cargo xtask gen`

--- completions/_inlyne.ps1
+++ completions/_inlyne.ps1
@@ -31,7 +31,7 @@ Register-ArgumentCompleter -Native -CommandName 'inlyne' -ScriptBlock {
             [CompletionResult]::new('--page-width', 'page-width', [CompletionResultType]::ParameterName, 'Maximum width of page in pixels')
             [CompletionResult]::new('-h', 'h', [CompletionResultType]::ParameterName, 'Print help')
             [CompletionResult]::new('--help', 'help', [CompletionResultType]::ParameterName, 'Print help')
-            [CompletionResult]::new('-V', 'V', [CompletionResultType]::ParameterName, 'Print version')
+            [CompletionResult]::new('-V', 'V ', [CompletionResultType]::ParameterName, 'Print version')
             [CompletionResult]::new('--version', 'version', [CompletionResultType]::ParameterName, 'Print version')
             break
         }
--- completions/inlyne.fish
+++ completions/inlyne.fish
@@ -1,4 +1,4 @@
-complete -c inlyne -s t -l theme -d 'Theme to use when rendering' -r -f -a "{auto	,dark	,light	}"
+complete -c inlyne -s t -l theme -d 'Theme to use when rendering' -r -f -a "{auto	'',dark	'',light	''}"
 complete -c inlyne -s s -l scale -d 'Factor to scale rendered file by [default: OS defined window scale factor]' -r
 complete -c inlyne -s c -l config -d 'Configuration file to use' -r -F
 complete -c inlyne -s w -l page-width -d 'Maximum width of page in pixels' -r
