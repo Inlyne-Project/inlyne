b1ca1deee53d70b38e79da14d38b78d9873f25e5
Cosmic Horror <CosmicHorrorDev@pm.me>
2024-03-25 21:30:14 -0600

    chore: Placate clippy

--- src/metrics/log_recorder.rs
+++ src/metrics/log_recorder.rs
@@ -36,34 +36,34 @@ impl Default for LogRecorder {
 impl metrics::Recorder for LogRecorder {
     fn describe_gauge(&self, key: KeyName, unit: Option<Unit>, _desc: SharedString) {
         let key = Key::from_name(key);
-        let gauge = self.0.get_or_create_histogram(&key, |g| Arc::clone(g));
+        let gauge = self.0.get_or_create_histogram(&key, Arc::clone);
         gauge.0.lock().unit = unit.unwrap_or(Unit::Count);
     }
 
     fn register_gauge(&self, key: &Key, _: &Metadata<'_>) -> Gauge {
-        let gauge = self.0.get_or_create_gauge(key, |g| Arc::clone(g));
+        let gauge = self.0.get_or_create_gauge(key, Arc::clone);
         Gauge::from_arc(gauge)
     }
 
     fn describe_counter(&self, key: KeyName, unit: Option<Unit>, _desc: SharedString) {
         let key = Key::from_name(key);
-        let counter = self.0.get_or_create_histogram(&key, |c| Arc::clone(c));
+        let counter = self.0.get_or_create_histogram(&key, Arc::clone);
         counter.0.lock().unit = unit.unwrap_or(Unit::Count);
     }
 
     fn register_counter(&self, key: &Key, _: &Metadata<'_>) -> Counter {
-        let counter = self.0.get_or_create_counter(key, |c| Arc::clone(c));
+        let counter = self.0.get_or_create_counter(key, Arc::clone);
         Counter::from_arc(counter)
     }
 
     fn describe_histogram(&self, key: KeyName, unit: Option<Unit>, _desc: SharedString) {
         let key = Key::from_name(key);
-        let hist = self.0.get_or_create_histogram(&key, |h| Arc::clone(h));
+        let hist = self.0.get_or_create_histogram(&key, Arc::clone);
         hist.0.lock().unit = unit.unwrap_or(Unit::Count);
     }
 
     fn register_histogram(&self, key: &Key, _: &Metadata<'_>) -> Histogram {
-        let hist = self.0.get_or_create_histogram(key, |h| Arc::clone(h));
+        let hist = self.0.get_or_create_histogram(key, Arc::clone);
         Histogram::from_arc(hist)
     }
 }
