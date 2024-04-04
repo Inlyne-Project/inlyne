bccefa6f1ccd291d483ffa4173e93e0d9cbc5d3d
CosmicHorror <CosmicHorrorDev@pm.me>
2024-04-03 21:45:59 -0600

    chore: Resolve beta clippy lints (#297)

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -811,7 +811,13 @@ impl HtmlInterpreter {
             }
         } else {
             if self.current_textbox.texts.is_empty() && self.state.text_options.pre_formatted == 0 {
-                str = str.trim_start().to_owned();
+                #[allow(
+                    unknown_lints, // Rust is still bad with back compat on new lints
+                    clippy::assigning_clones // Hit's a borrow-check issue. Needs a different impl
+                )]
+                {
+                    str = str.trim_start().to_owned();
+                }
             }
 
             let mut text = Text::new(str, self.hidpi_scale, text_native_color);
