70b4810e38b0aa974aa2ec5c6ea072a62f27ae33
Cosmic Horror <CosmicHorrorDev@pm.me>
2024-06-10 21:28:14 -0600

    Use our shiny new test utils

--- src/image/cache/tests.rs
+++ src/image/cache/tests.rs
@@ -18,7 +18,7 @@ use std::{
 use super::{L1Check, LayeredCache, LayeredCacheWorker, RemoteKey, SvgContext, TimeSource};
 use crate::{
     image::{cache::ImageError, ImageData},
-    test_utils::{self, HttpServer},
+    test_utils::{log, image, server},
 };
 
 use http::{header, HeaderMap, HeaderValue};
@@ -54,23 +54,24 @@ impl From<SystemTime> for FakeTimeSource {
 
 #[derive(Clone)]
 struct RemoteImage {
-    cache_control: CacheControl,
+    cache_control: server::CacheControl,
     path: &'static str,
-    content_type: ContentType,
+    content_type: server::ContentType,
     body: Vec<u8>,
 }
 
 impl RemoteImage {
-    fn from_sample(cache_control: CacheControl, path: &'static str, sample: Sample) -> Self {
+    fn from_sample(cache_control: server::CacheControl, path: &'static str, sample: image::Sample) -> Self {
         Self::new(cache_control, path, sample.into(), sample.pre_decode())
     }
 
     fn new(
-        cache_control: CacheControl,
+        cache_control: server::CacheControl,
         path: &'static str,
-        content_type: ContentType,
+        content_type: server::ContentType,
         body: Vec<u8>,
     ) -> Self {
+        dbg!(&cache_control);
         Self {
             cache_control,
             path,
@@ -80,254 +81,20 @@ impl RemoteImage {
     }
 }
 
-impl From<RemoteImage> for ResponseBox {
+impl From<RemoteImage> for server::File {
     fn from(image: RemoteImage) -> Self {
         let RemoteImage {
             cache_control,
-            path: _,
+            path,
             content_type,
             body,
         } = image;
-        Response::from_data(body)
-            .with_header(cache_control)
-            .with_header(content_type)
-            .boxed()
-    }
-}
-
-#[derive(Clone, Copy)]
-enum ContentType {
-    Gif,
-    Jpg,
-    Png,
-    Qoi,
-    Svg,
-    Webp,
-}
-
-impl ContentType {
-    fn to_str(self) -> &'static str {
-        match self {
-            Self::Gif => "image/gif",
-            Self::Jpg => "image/jpeg",
-            Self::Png => "image/png",
-            Self::Qoi => "image/qoi",
-            Self::Svg => "image/svg+xml",
-            Self::Webp => "image/webp",
+        Self {
+            url_path: path.into(),
+            mime: content_type,
+            cache_control: Some(cache_control),
+            bytes: body,
         }
-    }
-}
-
-impl From<Sample> for ContentType {
-    fn from(sample: Sample) -> Self {
-        match sample {
-            Sample::Gif(_) => Self::Gif,
-            Sample::Jpg(_) => Self::Jpg,
-            Sample::Png(_) => Self::Png,
-            Sample::Qoi(_) => Self::Qoi,
-            Sample::Svg(_) => Self::Svg,
-            Sample::Webp(_) => Self::Webp,
-        }
-    }
-}
-
-impl From<ContentType> for Header {
-    fn from(content_ty: ContentType) -> Self {
-        let header_name = header::CONTENT_TYPE.as_str().as_bytes();
-        let content_ty = content_ty.to_str().as_bytes();
-        Header::from_bytes(header_name, content_ty).unwrap()
-    }
-}
-
-#[derive(Clone, Default)]
-struct CacheControl {
-    immutable: bool,
-    max_age: Option<Duration>,
-}
-
-impl CacheControl {
-    fn new() -> Self {
-        Self::default()
-    }
-
-    fn immutable(mut self) -> Self {
-        self.immutable = true;
-        self
-    }
-
-    fn max_age(mut self, age: Duration) -> Self {
-        self.max_age = Some(age);
-        self
-    }
-
-    fn to_header_value(&self) -> Option<String> {
-        let CacheControl { immutable, max_age } = self;
-        let mut cache_control = Vec::new();
-        if *immutable {
-            cache_control.push("immutable".to_owned());
-        }
-        if let Some(age) = max_age {
-            cache_control.push(format!("max-age={}", age.as_secs()));
-        }
-
-        if !cache_control.is_empty() {
-            let cc = cache_control.join(",");
-            cc.parse().ok()
-        } else {
-            None
-        }
-    }
-}
-
-impl From<CacheControl> for Header {
-    fn from(cache_control: CacheControl) -> Self {
-        let value = cache_control.to_header_value().unwrap();
-        Self::from_bytes(header::CACHE_CONTROL.as_str(), value).unwrap()
-    }
-}
-
-impl From<CacheControl> for HeaderMap {
-    fn from(cache_control: CacheControl) -> Self {
-        let mut map = HeaderMap::new();
-
-        if let Some(value) = cache_control.to_header_value() {
-            let value = HeaderValue::from_str(&value).unwrap();
-            map.insert(header::CACHE_CONTROL, value);
-        }
-
-        map
-    }
-}
-
-#[derive(Clone, Copy)]
-enum Sample {
-    Gif(SampleGif),
-    Jpg(SampleJpg),
-    Png(SamplePng),
-    Qoi(SampleQoi),
-    Svg(SampleSvg),
-    Webp(SampleWebp),
-}
-
-impl From<SampleGif> for Sample {
-    fn from(gif: SampleGif) -> Self {
-        Self::Gif(gif)
-    }
-}
-
-impl From<SampleJpg> for Sample {
-    fn from(jpg: SampleJpg) -> Self {
-        Self::Jpg(jpg)
-    }
-}
-
-impl From<SamplePng> for Sample {
-    fn from(png: SamplePng) -> Self {
-        Self::Png(png)
-    }
-}
-
-impl From<SampleQoi> for Sample {
-    fn from(qoi: SampleQoi) -> Self {
-        Self::Qoi(qoi)
-    }
-}
-
-impl From<SampleSvg> for Sample {
-    fn from(svg: SampleSvg) -> Self {
-        Self::Svg(svg)
-    }
-}
-
-impl From<SampleWebp> for Sample {
-    fn from(webp: SampleWebp) -> Self {
-        Self::Webp(webp)
-    }
-}
-
-#[derive(Clone, Copy)]
-enum SampleGif {
-    AtuinDemo,
-    Rgb8,
-    Rgba8,
-}
-
-#[derive(Clone, Copy)]
-enum SampleJpg {
-    Rgb8,
-    Rgb8a,
-}
-
-#[derive(Clone, Copy)]
-enum SamplePng {
-    Ariadne,
-    Bun,
-    Rgb8,
-    Rgba8,
-}
-
-#[derive(Clone, Copy)]
-enum SampleQoi {
-    Rgb8,
-    Rgba8,
-}
-
-#[derive(Clone, Copy)]
-enum SampleSvg {
-    Corro,
-    Cargo,
-    Repology,
-}
-
-#[derive(Clone, Copy)]
-enum SampleWebp {
-    CargoPublicApi,
-}
-
-impl Sample {
-    fn pre_decode(self) -> Vec<u8> {
-        // TODO: swap these out for b64 encoded strings?
-        match self {
-            // TODO: move these includes to somewhere central?
-            Self::Jpg(jpg) => match jpg {
-                SampleJpg::Rgb8 => include_bytes!("../../../assets/test_data/rgb8.jpg").as_slice(),
-                SampleJpg::Rgb8a => {
-                    include_bytes!("../../../assets/test_data/rgba8.jpg").as_slice()
-                }
-            },
-            Self::Gif(gif) => match gif {
-                SampleGif::AtuinDemo => {
-                    include_bytes!("../../../assets/test_data/atuin_demo.gif").as_slice()
-                }
-                SampleGif::Rgb8 => todo!(),
-                SampleGif::Rgba8 => todo!(),
-            },
-            Self::Png(png) => match png {
-                SamplePng::Ariadne => {
-                    include_bytes!("../../../assets/test_data/ariadne_example.png").as_slice()
-                }
-                SamplePng::Bun => {
-                    include_bytes!("../../../assets/test_data/bun_logo.png").as_slice()
-                }
-                SamplePng::Rgb8 => todo!(),
-                SamplePng::Rgba8 => todo!(),
-            },
-            Self::Qoi(qoi) => match qoi {
-                SampleQoi::Rgb8 => todo!(),
-                SampleQoi::Rgba8 => todo!(),
-            },
-            Self::Svg(svg) => match svg {
-                SampleSvg::Corro => todo!(),
-                SampleSvg::Cargo => todo!(),
-                SampleSvg::Repology => todo!(),
-            },
-            Self::Webp(SampleWebp::CargoPublicApi) => todo!(),
-        }
-        .into()
-    }
-
-    fn post_decode(self) -> ImageData {
-        ImageData::load(&self.pre_decode(), true).unwrap()
     }
 }
 
@@ -342,22 +109,9 @@ fn in_memory_cache(time: FakeTimeSource) -> LayeredCacheWorker {
     LayeredCache::new_with_time(time, DUMMY_SVG_CTX).unwrap().in_memory()
 }
 
-fn image_server(images: Vec<RemoteImage>) -> HttpServer {
-    let images: Arc<[_]> = images.into();
-    HttpServer::spawn(images, |images, req| {
-        let not_found = Response::empty(404).boxed();
-        if req.method() != &Method::Get {
-            return not_found;
-        }
-
-        for image in images.iter() {
-            if image.path == req.url() {
-                return image.to_owned().into();
-            }
-        }
-
-        not_found
-    })
+fn image_server(images: Vec<RemoteImage>) -> server::MiniServerHandle {
+    let files = images.into_iter().map(Into::into).collect();
+    server::mock_file_server(files)
 }
 
 const ONE_HOUR: Duration = Duration::from_secs(60 * 60);
@@ -368,12 +122,12 @@ const ONE_DAY: Duration = Duration::from_secs(24 * 60 * 60);
 // Ensures that we can fetch a remote image from each layer of the cache
 #[test]
 fn layers() {
-    test_utils::init_test_log();
+    log::init();
 
     // Setup server
-    let image: Sample = SamplePng::Bun.into();
+    let image: image::Sample = image::SamplePng::Bun.into();
     let expected_data = image.post_decode();
-    let cache_control = CacheControl::new().immutable();
+    let cache_control = server::CacheControl::new().immutable();
     let remote_image = RemoteImage::from_sample(cache_control, "/sample.png", image);
     let server = image_server(vec![remote_image]);
     let url = server.url().to_owned() + "/sample.png";
@@ -423,7 +177,7 @@ fn layers() {
 
 #[test]
 fn mutli_client_mash() {
-    test_utils::init_test_log();
+    log::init();
 
     // TODO: This test is a stress-test verifying various assertions while having multiple
     // simulated clients simultaneously blasting away at the same global cache. The test goes
